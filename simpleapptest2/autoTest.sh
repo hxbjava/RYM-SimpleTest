7ea249c4c4da6e684f4caeee1a53fb30e7d68ebb
hxbjava <hxbjava@163.com>
2016-05-23 16:59:49 +0800

    修改sh脚本

--- simpleapptest2/autoTest.sh
+++ simpleapptest2/autoTest.sh
@@ -1,2 +1,4 @@
-#!/bin/sh
-appium
+#!/bin/bash
+sed "s/includes=\"testng-6.xml\"/includes=\"${xmlName}\"/g" build.xml>build.xml.tmp
+
+sed "s/value=\"/Users/rymtest/Desktop/app_package/\"/value=\"${app_package}\"/g" testng-6.xml>testng-6.xml.tmp
