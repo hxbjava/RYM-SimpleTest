cc1d8c851a52f434bdcc6d1a9a085ffa8c3c372a
hxbjava <hxbjava@163.com>
2016-05-24 15:15:58 +0800

    修改shell脚本

--- simpleapptest2/autoTest.sh
+++ simpleapptest2/autoTest.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-#sed "s/includes=\"testng-6.xml\"/includes=\"${xmlName}\"/g" build.xml>build.xml.tmp
-sed "s/value=\"\/Users\/rymtest\/Desktop\/app_package\/\"/value=\"${app_package}\"/g" testng-6.xml>testng-6.xml.tmp
+sed -i "" "s/value=\"\/Users\/rymtest\/Desktop\/app_package\/\"/value=\"${app_package}\"/g" testng-6.xml
