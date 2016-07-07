c1edcbbddd589519c10edac6a2e0c9701f891798
hxbjava <hxbjava@163.com>
2016-07-07 14:07:41 +0800

    修改shell脚本增加修改设备信息相关代码

--- simpleapptest2/autoTest.sh
+++ simpleapptest2/autoTest.sh
@@ -1,2 +1,5 @@
 sed -i "" "s/value=\"PAAnydoorSDK1130-1108.ipa\"/value=\"${appName}\"/g" testng-6.xml
 sed -i "" "s/value=\"\/Users\/rymtest\/Desktop\/app_package\/\"/value=\"${app_package}\"/g" testng-6.xml
+sed -i "" "s/value=\"娱乐 (2)\"/value=\"${deviceName}\"/g" testng-6.xml
+sed -i "" "s/value=\"9.0\"/value=\"${platformVersion}\"/g" testng-6.xml
+sed -i "" "s/value=\"8f023bbd000d58e34cfea2fdca1a8901d3123291\"/value=\"${udid}\"/g" testng-6.xml
