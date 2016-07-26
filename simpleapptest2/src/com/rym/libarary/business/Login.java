e70fb1cc8bbc94428c40bf316754d570460811fa
hxbjava <hxbjava@163.com>
2016-07-26 17:23:39 +0800

    更新login脚本启动卡死异常检测

--- simpleapptest2/src/com/rym/libarary/business/Login.java
+++ simpleapptest2/src/com/rym/libarary/business/Login.java
@@ -2,7 +2,11 @@ package com.rym.libarary.business;
 
 
 
+import java.util.List;
+
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.ios.IOSDriver;
 
 import org.openqa.selenium.By;
@@ -19,17 +23,29 @@ public class Login extends AutoTestBase{
 	/**
 	 * 选择appid 进入simple
 	 */
-	public static void startApp(String appID)
+	public static void startApp()
 	{
 		//腾讯gt的确定按钮
-		if(appOperate.waitForText(10, "确定"))
-		{
-			appOperate.click(driver.findElement(By.name("确定")), "点击 腾讯gt确认按钮");
-		}
-		appOperate.click(elements_StartPage_Host.HostAppIDSelect, "点击 appid  进行选择");
-		appOperate.click(driver.findElement(By.name(appID)), " appid  选择为 "+appID);
+		Log.logInfo("启动app，选择任意门进入simple");
+		if(platformName.toLowerCase().contains("ios"))
+        {
+			if(appOperate.waitForText(10, "确定"))
+			{
+				appOperate.click(driver.findElement(By.name("确定")), "点击 腾讯gt确认按钮");
+				
+			}
+        }
+			appOperate.click(elements_StartPage_Host.HostAppIDSelect, "点击 appid  进行选择");
+			if(platformName.toLowerCase().contains("android"))
+            {
+				appOperate.swipeToUp(1000);
+				Sleep.sleep(5);
+            }
+			appOperate.click(elements_StartPage_Host.HostAppIDclick, " 选定 appid   ");
+
 		appOperate.click(elements_StartPage_Host.HostStartClick, "点击  进入页面 ");
-		Sleep.sleep(3);
+
+		Sleep.sleep(5);
 	}
 	
 	/**
@@ -37,14 +53,23 @@ public class Login extends AutoTestBase{
 	 */
 	public static void loginyztByH5(String login_name,String login_password)
 	{
-		 if(appOperate.waitForText(20, "一账通号/手机号/身份证号/邮箱"))
+		 if(appOperate.waitForText(20, "一账通登录"))
     	 {
     		 Log.logInfo("已弹出登录");
+    		 if(platformName.toLowerCase().contains("android"))
+             {
+    			 List<WebElement> list=driver.findElements(By.className("android.widget.EditText"));
+    			 list.get(0).click();
+    			 list.get(0).sendKeys("aaaaaa");
+    			 Sleep.sleep(20);
+             }
+    		 else{
 			appOperate.click(elements_PersonalPage.ClickAccount, "点击 账号 进行输入");
 			appOperate.sendKeys(elements_PersonalPage.ClickAccount, "输入 账号", login_name);
 		 	appOperate.click(elements_PersonalPage.ClickPassword, "点击 密码 进行输入");
 			appOperate.sendKeys(elements_PersonalPage.ClickPassword, "输入 密码", login_password);
 		 	appOperate.hideKeyboard();
+		 	}
 		 	Sleep.sleep(2);
 		 	appOperate.click(elements_PersonalPage.Clicklogin, "找到［登 录］按钮，并点击!");
     	 }
@@ -94,13 +119,16 @@ public class Login extends AutoTestBase{
 			 {
 				 appOperate.sendKeys(driver.findElement(By.xpath("//UIAPickerWheel")), "输入宿主账号", login_HostName);
 				 appOperate.click(driver.findElement(By.name("确认")), "ios 点击 确认选择用户");
+			 }else
+			 {
+				 appOperate.click(driver.findElement(By.name(login_HostName)), "点击 输入宿主账号");
 			 }
 			 appOperate.click(elements_PersonalPage.ClickHostLogin, "点击登陆");
     	 }
 		  Sleep.sleep(10);
 		 while(true)
 		 {
-			 if(appOperate.waitForText(20, login_HostName))
+			 if(appOperate.waitForText(10, login_HostName))
 	    	 {	
 				 appOperate.click(elements_PersonalPage.ClickHostLogin, "点击登陆");
 				 Sleep.sleep(10);
@@ -114,11 +142,21 @@ public class Login extends AutoTestBase{
 	 */
 	public static void loginNoyztByHost(String login_HostName)
 	{
+		int ii=0;
 		 while(true)
 		 {
+			 if(ii>5)
+			 {
+				 Log.logInfo("启动卡死");
+				 break;
+			 }
 			 if(appOperate.waitForText(20, "非一账通"))
 	    	 {	
 				 appOperate.click(driver.findElement(By.name("非一账通")), "点击 非一账通");
+				 if(appOperate.waitForText(5, "烦死我了"))
+		    	 {	
+					 appOperate.click(driver.findElement(By.name("烦死我了")), "点击 烦死我了");
+		    	 }
 				 appOperate.click(elements_PersonalPage.ClickByHost, "点击 宿主登陆");
 				 if(appOperate.waitForText(20, "返回"))
 		    	 {	
@@ -127,6 +165,9 @@ public class Login extends AutoTestBase{
 					 {
 						 appOperate.sendKeys(driver.findElement(By.xpath("//UIAPickerWheel")), "输入宿主账号", login_HostName);
 						 appOperate.click(driver.findElement(By.name("确认")), "ios 点击 确认选择用户");
+					 }else
+					 {
+						 appOperate.click(driver.findElement(By.name(login_HostName)), "点击 输入宿主账号");
 					 }
 					 appOperate.click(elements_PersonalPage.ClickHostLogin, "点击登陆");
 					 break;
@@ -134,12 +175,13 @@ public class Login extends AutoTestBase{
 		//		 Log.logInfo("页面元素如下："+driver.getPageSource());
 			//	 appOperate.click(driver.findElement(By.xpath("//*[@value='"+login_HostName+"']")), "选择宿主登陆账号");
 				 
-	    	 }
+	    	 }else
+	    	 {  ii=ii+1;}
 		 }
 		  Sleep.sleep(20);
 			 while(true)
 			 {
-				 if(appOperate.waitForText(20, login_HostName))
+				 if(appOperate.waitForText(10, login_HostName))
 		    	 {	
 					 Log.logInfo("你不应该进来的");
 					 appOperate.click(elements_PersonalPage.ClickHostLogin, "点击登陆");
