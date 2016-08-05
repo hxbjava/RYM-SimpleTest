d0ec945438be932702568860a610d63f1a274750
hxbjava <hxbjava@163.com>
2016-08-05 11:28:51 +0800

    修改shell脚本

--- simpleapptest2/autoTest.sh
+++ simpleapptest2/autoTest.sh
@@ -3,4 +3,4 @@ sed -i "" "s/value=\"\/Users\/rymtest\/Desktop\/app_package\/\"/value=\"${app_pa
 sed -i "" "s/value=\"娱乐 (2)\"/value=\"${deviceName}\"/g" testng-6.xml
 sed -i "" "s/value=\"9.0\"/value=\"${platformVersion}\"/g" testng-6.xml
 sed -i "" "s/value=\"8f023bbd000d58e34cfea2fdca1a8901d3123291\"/value=\"${udid}\"/g" testng-6.xml
-sed -i "" "s/value=\"testng-6.xml\"/value=\"${testngxml}\"/g" build.xml
+sed -i "" "s/includes=\"testng-6.xml\"/includes=\"${testngxml}\"/g" build.xml
