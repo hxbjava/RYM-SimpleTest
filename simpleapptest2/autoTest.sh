22c12122e8b00463cfaf5ac26f795a0849a82056
hxbjava <hxbjava@163.com>
2016-08-08 15:26:26 +0800

    增加延迟等待时间

--- simpleapptest2/autoTest.sh
+++ simpleapptest2/autoTest.sh
@@ -1,3 +1,4 @@
+sleep 60
 sed -i "" "s/value=\"PAAnydoorSDK1130-1108.ipa\"/value=\"${appName}\"/g" testng-6.xml
 sed -i "" "s/value=\"\/Users\/rymtest\/Desktop\/app_package\/\"/value=\"${app_package}\"/g" testng-6.xml
 sed -i "" "s/value=\"娱乐 (2)\"/value=\"${deviceName}\"/g" testng-6.xml
