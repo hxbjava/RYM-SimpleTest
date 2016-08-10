929e3187858fb759421de18e408eb9276bf36818
hxbjava <hxbjava@163.com>
2016-08-10 15:43:11 +0800

    增加修改android testng.xml配置脚本

--- simpleapptest2/autoTest.sh
+++ simpleapptest2/autoTest.sh
@@ -4,3 +4,4 @@ sed -i "" "s/value=\"娱乐 (2)\"/value=\"${deviceName}\"/g" testng-6.xml
 sed -i "" "s/value=\"9.0\"/value=\"${platformVersion}\"/g" testng-6.xml
 sed -i "" "s/value=\"8f023bbd000d58e34cfea2fdca1a8901d3123291\"/value=\"${udid}\"/g" testng-6.xml
 sed -i "" "s/includes=\"testng-6.xml\"/includes=\"${testngxml}\"/g" build.xml
+sed -i "" "s/value=\"\/Users\/rymtest\/Desktop\/app_package\/\"/value=\"${app_package}\"/g" testng-android-rongyao6.xml
